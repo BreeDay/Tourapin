d83644383f96cdf9cbd04855914af2025a04d834
BreeDay <breeonad@gmail.com>
2022-06-08 11:14:29 -1000

    Initial commit.

---
+++ src/Tourapin_assets/src/index.js
@@ -0,0 +1,19 @@
+import { Tourapin } from "../../declarations/Tourapin";
+
+document.querySelector("form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const button = e.target.querySelector("button");
+
+  const name = document.getElementById("name").value.toString();
+
+  button.setAttribute("disabled", true);
+
+  // Interact with foo actor, calling the greet method
+  const greeting = await Tourapin.greet(name);
+
+  button.removeAttribute("disabled");
+
+  document.getElementById("greeting").innerText = greeting;
+
+  return false;
+});
